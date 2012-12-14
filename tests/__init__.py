ff52915d420913badcb9a9fa04f4ea9aaa7f7473
Jérome Perrin <jerome@nexedi.com>
2012-12-14 15:11:42 +0100

    return a different format status dict for functional tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -72,7 +72,30 @@ class ERP5(_ERP5):
       if not status_dict['status_code']:
         status_dict = self.runUnitTest('--load', '--activity_node=2', test)
       return status_dict
+    if test.startswith('testFunctional'):
+      return self._updateFunctionalTestResponse(self.runUnitTest(test))
     return super(ERP5, self).run(test)
+
+  def _updateFunctionalTestResponse(self, status_dict):
+    """ Convert the Unit Test output into more accurate information
+        related to funcional test run.
+    """
+    # Parse relevant information to update response information
+    try:
+      summary, html_test_result = status_dict['stderr'].split("-"*79)[1:3]
+    except ValueError:
+      # In case of error when parse the file, preserve the original
+      # informations. This prevents we have unfinished tests.
+      return status_dict
+    status_dict['html_test_result'] = html_test_result
+    search = self.FTEST_PASS_FAIL_RE.search(summary)
+    if search:
+      group_dict = search.groupdict()
+      status_dict['failure_count'] = int(group_dict['failures'])
+      status_dict['test_count'] = int(group_dict['total'])
+      status_dict['skip_count'] = int(group_dict['expected_failure'])
+    return status_dict
+
 
 class ERP5_simulation(_ERP5):
 
